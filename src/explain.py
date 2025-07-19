2a2aeda13530734ddd48fb7fae82ab57010eea01
Ujan Pradhan <113442287+2003UJAN@users.noreply.github.com>
2025-07-20 02:41:22 +0530

    Update explain.py

--- src/explain.py
+++ src/explain.py
@@ -2,10 +2,9 @@ import shap
 import matplotlib.pyplot as plt
 
 def explain_model(model, X_train):
-    explainer = shap.Explainer(model, X_train)
+    explainer = shap.Explainer(model)
     shap_values = explainer(X_train)
-    
-    # Plot summary
-    shap.summary_plot(shap_values, X_train, show=False)
-    plt.savefig("shap_summary.png", bbox_inches='tight')
 
+    shap.summary_plot(shap_values, X_train, show=False)
+    plt.tight_layout()
+    plt.savefig("shap_summary.png", bbox_inches='tight')
