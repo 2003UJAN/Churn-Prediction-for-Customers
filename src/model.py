fdf73734ee07c7242bb3f27407376d8b59835c80
Ujan Pradhan <113442287+2003UJAN@users.noreply.github.com>
2025-07-20 02:41:06 +0530

    Update model.py

--- src/model.py
+++ src/model.py
@@ -3,7 +3,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report
 
 def train_models(X_train, y_train):
-    xgb = XGBClassifier(use_label_encoder=False, eval_metric='logloss')
+    xgb = XGBClassifier(use_label_encoder=False, eval_metric='logloss', random_state=42)
     xgb.fit(X_train, y_train)
 
     logreg = LogisticRegression(max_iter=1000)
@@ -15,4 +15,3 @@ def evaluate(model, X_test, y_test):
     preds = model.predict(X_test)
     report = classification_report(y_test, preds, output_dict=True)
     return report
-
