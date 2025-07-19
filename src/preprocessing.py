b9b7cb7817d24a92b292cfb639287e51227356a0
Ujan Pradhan <113442287+2003UJAN@users.noreply.github.com>
2025-07-20 02:40:51 +0530

    Update preprocessing.py

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -4,23 +4,25 @@ from sklearn.preprocessing import LabelEncoder
 
 def load_and_preprocess(path="data/customer_churn.csv"):
     df = pd.read_csv(path)
-    df.drop(columns=['customerID'], inplace=True)
-    df.replace(" ", pd.NA, inplace=True)
-    df.dropna(inplace=True)
-    
-    # Convert total charges to numeric
-    df["TotalCharges"] = pd.to_numeric(df["TotalCharges"])
 
-    # Encode target
-    df["Churn"] = df["Churn"].map({"Yes": 1, "No": 0})
-    
-    # Encode categorical
+    # Drop customerID
+    df.drop(columns=['customerID'], inplace=True)
+
+    # Convert TotalCharges to numeric
+    df['TotalCharges'] = pd.to_numeric(df['TotalCharges'], errors='coerce')
+    df.dropna(inplace=True)
+
+    # Encode target column
+    df['Churn'] = df['Churn'].map({'Yes': 1, 'No': 0})
+
+    # Encode all categorical columns
     label_encoders = {}
-    for col in df.select_dtypes(include="object").columns:
+    for col in df.select_dtypes(include='object').columns:
         le = LabelEncoder()
         df[col] = le.fit_transform(df[col])
         label_encoders[col] = le
 
     X = df.drop("Churn", axis=1)
     y = df["Churn"]
+
     return train_test_split(X, y, test_size=0.2, random_state=42)
